a4fdda824ec7cb8efe13b2423f730f7fa39dc95d
vikas1509 <119025906+vikas1509@users.noreply.github.com>
2023-07-31 23:53:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-function volume_sphere() {
-    //Write your code here
-	onsubmit.preventDefault();
-	let r =document.getElementById("radius").value;
-	let ans =r;
+function volume_sphere(event) {
+  event.preventDefault(); // This prevents the form submission from reloading the page
 
-	let vol=document.getElementById("volume");
-	vol.value=ans;
-	
-  
-} 
+  let r = document.getElementById("radius").value;
+  let ans = (4 / 3) * Math.PI * Math.pow(r, 3);
+
+  let vol = document.getElementById("volume");
+  vol.value = ans.toFixed(2);
+}
 
 
 window.onload = document.getElementById("MyForm").onsubmit = volume_sphere;
