6ba30b118a341f6ba2741e9b1f596621bb4b3160
vikas1509 <119025906+vikas1509@users.noreply.github.com>
2023-07-31 23:51:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function volume_sphere() {
     //Write your code here
+	onsubmit.preventDefault();
+	let r =document.getElementById("radius").value;
+	let ans =r;
+
+	let vol=document.getElementById("volume");
+	vol.value=ans;
+	
   
 } 
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+
+window.onload = document.getElementById("MyForm").onsubmit = volume_sphere;
